63cf597b2717b40665cff016c91722d9a7d55e62
Peter <cirediatpl@gmail.com>
2019-07-02 10:52:10 -0400

    created group join feature

---
+++ app/controllers/usergroups_controller.rb
@@ -0,0 +1,14 @@
+class UsergroupsController < ApplicationController
+
+    def create
+        usergroup = Usergroup.create(usergroup_params)
+        redirect_to user_path(usergroup.user)
+    end
+
+    private
+
+    def usergroup_params
+        params.require(:usergroup).permit(:user_id, :group_id)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :groups
   resources :quizzes
   resources :users
+  resources :usergroups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/signup", to: "users#new"
   get "/login", to: "sessions#new"
